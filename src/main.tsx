eefc644418283ba2124f2633b0d3276ea99503dd
christopher pinkston <98330612+chrisPinkston@users.noreply.github.com>
2025-09-17 23:22:23 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom'
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+/ Vite exposes the base path here:
+const basename = import.meta.env.BASE_URL; // -> '/daily-grid-quest/'
+
+
+createRoot(document.getElementById("root")!).render(<BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>);
