5141b4ca188acf6f5da0cf750ef4cc5044f018cb
christopher pinkston <98330612+chrisPinkston@users.noreply.github.com>
2025-09-17 22:22:37 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:'/daily-grid-quest',
+  base:'/daily-grid-quest/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
